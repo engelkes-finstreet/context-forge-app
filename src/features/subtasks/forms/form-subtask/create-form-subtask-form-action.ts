adbef5e49a8ef44b367a0201467bc3128d5439c9
Patrick Engelkes <patrick.engelkes@finstreet.de>
2025-10-29 11:46:20 +0100

    Submit form-subtask action.

--- src/features/subtasks/forms/form-subtask/create-form-subtask-form-action.ts
+++ src/features/subtasks/forms/form-subtask/create-form-subtask-form-action.ts
@@ -2,11 +2,40 @@
 
 import { FormState } from "@/components/forms/types";
 import { CreateFormSubtaskFormInput } from "@/features/subtasks/forms/form-subtask/create-form-subtask-form-schema";
+import { routes, typedRedirect } from "@/lib/routes";
+import { SubtaskService } from "@/lib/services/subtask-service";
+import { TaskService } from "@/lib/services/task-service";
+import { SubtaskType } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 
 export async function createFormSubtaskFormAction(
   state: FormState,
   formData: CreateFormSubtaskFormInput,
 ): Promise<FormState> {
-  console.log({ formData: JSON.stringify(formData, null, 2) });
-  return { error: null, message: "Subtask created successfully" };
+  const task = await TaskService.getTaskById(formData.taskId);
+
+  const metadata = {
+    fields: formData.fields,
+  };
+
+  const result = await SubtaskService.createSubtask({
+    taskId: formData.taskId,
+    name: formData.subtaskName,
+    type: SubtaskType.FORM,
+    content: "",
+    metadata: JSON.stringify(metadata),
+  });
+
+  if (result.success) {
+    revalidatePath(`/projects/${task.projectId}/tasks/${task.id}`);
+    typedRedirect(routes.projects.tasks.detail, {
+      projectId: task.projectId,
+      taskId: task.id,
+    });
+  } else {
+    return {
+      error: result.errorMessage,
+      message: null,
+    };
+  }
 }
